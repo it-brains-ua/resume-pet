61a8bf2424e086313ee523d357b421f4e5868576
chip22294 <chip22294@gmail.com>
2023-05-10 11:44:47 +0300

    Lesson is over. Home work complete.

--- src/route/index.js
+++ src/route/index.js
@@ -12,7 +12,9 @@ router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
+  res.render('index', {
+    title: 'Pet resume',
+  })
   //                  ↑↑ сюди вводимо JSON дані
 })
 
@@ -23,6 +25,32 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+    // title: 'Pet resume summary',
+    person: {
+      name: {
+        firstname: 'Eugene',
+        lastname: 'Nechiporuk',
+      },
+      position: 'Junior Fullstack JS Developer',
+      sallary: '600$ в місяц',
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
+        work on a new project I learn the domain and try to understand the idea of the project. Good team
+        player, every colleague is a friend to me.`,
+      },
+      otherexp: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
+          tournament position, goals etc), analyzing by simple mathematics models and preparing probability
+          for such events like: money line - first win / draw / second win, totals etc.`,
+      },
+      social: {
+        email: 'chip@example.com',
+        phone: '+380977541536',
+        linkedin: 'https://www.linkedin.com/in/chip22294/',
+      },
+    },
   })
 })
 
