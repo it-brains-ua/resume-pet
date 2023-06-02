d49c33a60820e55762b84c1b1036aa639d1fc7f6
PavelPKhUA <PavelPKhUA@gmail.com>
2023-06-02 16:52:47 +0300

    module3homework1

--- src/route/index.js
+++ src/route/index.js
@@ -23,6 +23,39 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+    title: 'Resume',
+    name: {
+      firstname: 'Ivan',
+      lastname: 'Ivanov',
+    },
+    position: 'Junior Fullstack JS Developer',
+
+    salary: '$600 в місяць',
+
+    summary: {
+      title: 'Summary',
+      text: ' Open-minded for new technologies, with 1 years of experience in development. Whenever I start to work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.',
+    },
+
+    experience: {
+      title: 'Other experience',
+      text: 'Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics ( tournament position, goals etc), analyzing by simple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc.',
+    },
+
+    social: {
+      email: {
+        text: 'ivan@mail.com',
+        href: 'mailto:ivan@mail.com',
+      },
+      phone: {
+        text: '+380670000123',
+        href: 'tel:+380670000123',
+      },
+      linked: {
+        text: 'LinkedIn',
+        href: 'https://www.linkedin.com/in/dmytro-test',
+      },
+    },
   })
 })
 
