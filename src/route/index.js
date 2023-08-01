0db23eccb6f1ecc5fd8b348c4cf7ba21561d1155
SelmXP <selmxp@gmail.com>
2023-08-02 01:07:25 +0300

    2 урок-завершений

--- src/route/index.js
+++ src/route/index.js
@@ -23,49 +23,107 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
-    title: 'Resume',
 
-    name: {
-      firsname: 'Ivan',
-      lastname: 'Ivanov',
+    page: {
+      title: 'Resume',
     },
 
-    position: 'Junior Fullstack JS Developer',
+    header: {
+      name: {
+        firstname: 'Ivan',
+        lastname: 'Ivanov',
+      },
+      position: 'Junior Fullstack JS Developer',
+      salary: '600$ в місяць',
+    },
 
-    social: {
-      email: {
-        text: 'dmytro@mail.com',
-        href: 'mailto:dmytro@mail.com',
+    main: {
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
+        work on a new project I learn the domain and try to understand the idea of the project. Good team
+        player, every colleague is a friend to me.`,
       },
-      phone: {
-        text: '+380670000123',
-        href: 'tel:+380670000123',
-      },
-      linkedin: {
-        text: 'LinkedIn',
-        href: 'https://www.linkedin.com/in/dmytro-test',
+
+      experience: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
+          tournament position, goals etc), analyzing by simple mathematics models and preparing probability
+          for such events like: money line - first win / draw / second win, totals etc.`,
       },
     },
 
-    salary: '600$ в місяць',
-
-    summary: {
-      title: 'Summary',
-      text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
-      work on a new project I learn the domain and try to understand the idea of the project. Good team
-      player, every colleague is a friend to me.`,
-    },
-
-    experience: {
-      title: 'Other experience',
-      text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
-        tournament position, goals etc), analyzing by simple mathematics models and preparing probability
-        for such events like: money line - first win / draw / second win, totals etc.`,
+    footer: {
+      social: {
+        email: {
+          text: 'dmytro@mail.com',
+          href: 'mailto:dmytro@mail.com',
+        },
+        phone: {
+          text: '+380670000123',
+          href: 'tel:+380670000123',
+        },
+        linkedin: {
+          text: 'LinkedIn',
+          href: 'https://www.linkedin.com/in/dmytro-test',
+        },
+      },
     },
   })
 })
 
 // ================================================================
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume',
+    },
+
+    header: {
+      name: {
+        firstname: 'Ivan',
+        lastname: 'Ivanov',
+      },
+      position: 'Junior Fullstack JS Developer',
+      salary: '600$ в місяць',
+    },
+
+    main: {
+      skills: [
+        'HTML',
+        'Handlebars',
+        'VS Code',
+        'Git',
+        'Terminal',
+        'NPM',
+      ],
+    },
+
+    footer: {
+      social: {
+        email: {
+          text: 'dmytro@mail.com',
+          href: 'mailto:dmytro@mail.com',
+        },
+        phone: {
+          text: '+380670000123',
+          href: 'tel:+380670000123',
+        },
+        linkedin: {
+          text: 'LinkedIn',
+          href: 'https://www.linkedin.com/in/dmytro-test',
+        },
+      },
+    },
+  })
+})
 
 // Підключаємо роутер до бек-енду
 module.exports = router
