31532c1a6abc2acca89f515b214689f05bd2ecea
EvgPro333 <tatyaakaunt@gmail.com>
2023-09-03 08:27:45 +0300

    lesson1

--- src/route/index.js
+++ src/route/index.js
@@ -23,6 +23,27 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+    // title: 'Dmytro Ivanov | Resume',
+    title: 'resume',
+    name: {
+      firstname: 'Dmytro',
+      lastname: 'Ivanov',
+    },
+    position: 'Junior Fullstack JS Developer',
+    social: {
+      email: {
+        text: 'dmytro@mail.com',
+        href: 'mailto:dmytro@mail.com',
+      },
+      phone: {
+        text: '+380670000123',
+        href: 'tel:+380670000123',
+      },
+      LinkedIn: {
+        text: 'LinkedIn',
+        href: 'https://www.linkedin.com/in/dmytro-test',
+      },
+    },
   })
 })
 
